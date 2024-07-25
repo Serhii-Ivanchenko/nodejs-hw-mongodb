e6d17c13feb641df80f9f4b93186649cb5a6c335
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-07-25 18:38:24 +0300

    add err processing

--- src/server.js
+++ src/server.js
@@ -16,13 +16,25 @@ export const setupServer = () => {
     }),
   );
 
-    app.use(cors());
+  app.use(cors());
 
-    app.use('*', (req, res, next) => {
-      res.status(404).json({
-        message: 'Not found',
-      });
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
     });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+    });
+  });
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
