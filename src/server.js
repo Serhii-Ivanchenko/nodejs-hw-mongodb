b24de0d7630f81487accea2f1f86b5134d383105
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-07-25 19:36:31 +0300

    step 6 done (getting contact by Id)

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { getAllContacts } from './services/contacts.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -32,6 +32,24 @@ export const setupServer = () => {
       status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const contactId = req.params.contactId;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,4 +4,6 @@ export const getAllContacts = async () => {
   return await ContactsCollection.find();
 };
 
-
+export const getContactById = async (id) => {
+  return await ContactsCollection.findById(id);
+};
