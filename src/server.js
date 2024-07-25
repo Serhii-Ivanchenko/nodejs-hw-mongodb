8c4f9b0e18a8df218ea1fc394b8d4ffaf2192d84
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-07-25 19:03:20 +0300

    step 3 done (MongoDB connection)

--- src/server.js
+++ src/server.js
@@ -33,6 +33,7 @@ export const setupServer = () => {
   app.use((err, req, res, next) => {
     res.status(500).json({
       message: 'Something went wrong',
+      error: err.message,
     });
   });
 
