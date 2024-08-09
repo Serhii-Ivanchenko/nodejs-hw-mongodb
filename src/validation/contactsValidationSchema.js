cfe3ad77beef4867de82296c2afa06e730e3dc87
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-08-09 16:02:36 +0300

    step-2 done (add validation of body and id)

---
+++ src/validation/contactsValidationSchema.js
@@ -0,0 +1,47 @@
+import Joi from 'joi';
+
+export const createContactsValidationSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Name should be a string!',
+    'string.min': 'Name should have at least {#limit} characters',
+    'string.max': 'Name should have at most {#limit} characters',
+    'any.required': 'Name is required',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'phoneNumber should be a string!',
+    'string.min': 'phoneNumber should have at least {#limit} characters',
+    'string.max': 'phoneNumber should have at most {#limit} characters',
+    'any.required': 'phoneNumber is required',
+  }),
+  email: Joi.string().email().required().messages({
+    'string.base': 'Email should be a string!',
+    'any.required': 'Email is required',
+  }),
+  isFavourite: Joi.boolean().required().messages({
+    'any.required': 'Field "isFavourite" is required',
+  }),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .required()
+    .messages({
+      'any.required': 'ContactType is required',
+    }),
+});
+
+export const updateContactsValidationSchema = Joi.object({
+  name: Joi.string().min(3).max(20).messages({
+    'string.base': 'Name should be a string!',
+    'string.min': 'Name should have at least {#limit} characters',
+    'string.max': 'Name should have at most {#limit} characters',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).messages({
+    'string.base': 'phoneNumber should be a string!',
+    'string.min': 'phoneNumber should have at least {#limit} characters',
+    'string.max': 'phoneNumber should have at most {#limit} characters',
+  }),
+  email: Joi.string().email().messages({
+    'string.base': 'Email should be a string!',
+  }),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
